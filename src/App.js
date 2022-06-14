3b8a2dface279fe568ff0e2d13ac6db8779dbc03
Skywalker <you@example.com>
2022-06-14 16:04:47 +0400

    file system

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Main from "./components/Main";
+
+import "./reset.css";
+
+
+function App() {
+  return (
+    <div className="app">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
