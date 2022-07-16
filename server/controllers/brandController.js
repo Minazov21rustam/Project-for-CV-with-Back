48f1c6643deb3d111923372a69ca50cc5f259a4b
Skywalker <you@example.com>
2022-07-16 19:04:37 +0400

    44:41

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -5,7 +5,10 @@ class BrandController {
     const brand = await Brand.create({ name });
     return res.json(brand);
   }
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    const brands = await Brand.findAll()
+    return res.json(brands)
+  }
 }
 
 module.exports = new BrandController();
--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,5 +1,21 @@
+const uuid = require("uuid");
+const path = require("path");
+const { Device } = require("../models/models");
 class DeviceController {
-  async create(req, res) {}
+  async create(req, res) {
+    const { name, price, brandId, typeId, info } = req.body;
+    const { img } = req.files;
+    let fileName = uuid.v4() + ".jpg";
+    img.mv(path.resolve(__dirname, "..", "static", fileName));
+
+    const device = await Device.create({
+      name,
+      price,
+      brandId,
+      typeId,
+      img: fileName,
+    });
+  }
   async getAll(req, res) {}
   async getOne(req, res) {}
 }
